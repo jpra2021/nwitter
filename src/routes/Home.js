a7708e33c3e663f9975189c4d23252e275ddf8cc
jpra2021 <jthvmcm2021@cuk.edu>
2022-08-10 17:08:54 +0900

    3.1 Form and DB setup

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [nWeet, setNweet] = useState("");
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={nWeet}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={140}
+        />
+        <input type="submit" value="Tweet" />
+      </form>
+    </div>
+  );
+};
 //will activate when it called
 
 export default Home;
