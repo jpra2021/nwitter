8994131003785fa021ea804859206bc5366ff97b
jpra2021 <jthvmcm2021@cuk.edu>
2022-08-10 19:48:42 +0900

    3.1 nwitting, location error

--- src/fbase.js
+++ src/fbase.js
@@ -5,19 +5,20 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_APIKEY,
-  authDomain: process.env.REACT_APP_AUTHODOMAIN,
-  projectId: process.env.REACT_APP_AUTHODOMAIN,
-  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_APPID,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // (1)Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
+const App = initializeApp(firebaseConfig);
 //for first checking in index console.log, and it will not be used again but ....? -> it will be used in getAuth
 //-export default app; (first)
 
 //v9
 //export const authService = getAuth(firebaseApp);
 export const authService = getAuth();
+export const dbService = getFirestore();
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,20 @@
+import { dbService } from "fbase";
 import React, { useState } from "react";
+import { collection, addDoc } from "firebase/firestore";
 
 const Home = () => {
-  const [nWeet, setNweet] = useState("");
-  const onSubmit = (event) => {
+  const [nweet, setNweet] = useState("");
+  const onSubmit = async (event) => {
     event.preventDefault();
+    console.log("onsubmit작동");
+    await addDoc(collection(dbService, "nweets"), {
+      nweet: nweet,
+      createdAt: Date.now(),
+    });
+    console.log("됨?");
+    setNweet("");
   };
+
   const onChange = (event) => {
     const {
       target: { value },
@@ -16,7 +26,7 @@ const Home = () => {
       <form onSubmit={onSubmit}>
         <input
           onChange={onChange}
-          value={nWeet}
+          value={nweet}
           type="text"
           placeholder="What's on your mind?"
           maxLength={140}
